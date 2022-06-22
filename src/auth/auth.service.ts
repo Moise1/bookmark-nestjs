1645b8428836f2e01b8f31a0dfef34e6bd865fc1
Moise1 <rwibutso.mo@gmail.com>
2022-06-22 11:09:27 +0200

    user authentication & validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,52 +1,50 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import {Injectable, ConflictException, HttpStatus, HttpException} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon from 'argon2';
-import { PrismaOpsService } from "src/prisma-ops/prisma-ops.service";
-import { AuthDto } from "./dto";
+import { PrismaOpsService } from 'src/prisma-ops/prisma-ops.service';
+import { AuthDto } from './dto';
 @Injectable()
-export class AuthService{
-    constructor(private prismaOpsService: PrismaOpsService){}
-    
-    async login(body: AuthDto){
-        try {
-            const user = await this.prismaOpsService.user.findUnique({
-                where: { email: body.email}
-            });
+export class AuthService {
+  constructor(private prismaOpsService: PrismaOpsService) {}
 
-            console.log('THE LOGGED IN USER', user);
-            if(!user){
-                throw new ForbiddenException('Sorry! Creds are incorrect.');
-            }
+  async login(body: AuthDto) {
+    try {
+      const user = await this.prismaOpsService.user.findUnique({
+        where: { email: body.email }
+      });
 
-            const pswdMatch = await argon.verify(user.hash, body.password);
+      if (!user) throw new HttpException('Sorry! User not found.', HttpStatus.NOT_FOUND)
 
-            if(!pswdMatch) throw new ForbiddenException('Sorry! Creds are incorrect.');
+      const pswdMatch = await argon.verify(user.hash, body.password);
+      
+      if (!pswdMatch) throw new HttpException('Sorry! Incorrect creds.', HttpStatus.BAD_REQUEST);
+      
+      return {
+          message: 'Successfully logged in',
+          user
+      }
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+    }
+  }
 
-            return user;
-        } catch (error) {
-            
-        }
-    };
-
-    async register(body: AuthDto){
-       try {
-        const hash =  await argon.hash(body.password);
-        const user = await this.prismaOpsService.user.create({
-            data: {
-                email: body.email,
-                hash
-            },
-        })
-        return user;
-       } catch (error) {
-           if(error instanceof PrismaClientKnownRequestError){
-               if(error.code === 'P2002') throw new ForbiddenException('Creds already taken.');
-               throw error;
-
-           };
-       }
-    };
-
+  async register(body: AuthDto) {
+    try {
+      const hash = await argon.hash(body.password);
+      const user = await this.prismaOpsService.user.create({
+        data: {
+          email: body.email,
+          hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') throw new ConflictException('Email already taken.');
+        throw error;
+      }
+    }
+  }
 }
-
-// Stopped at 1:17:00
